845cd5ae7020dc71c60cc01c54b2a0e850de99a4
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-09 06:30:27 +0300

    Add Card.jsx, default image on error

---
+++ src/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card(link) {
+  const fallback = "./img/no_image.svg";
+  return (
+    <button className="card">
+      <img src="./no_image.svg" alt="" />
+    </button>
+  );
+}
