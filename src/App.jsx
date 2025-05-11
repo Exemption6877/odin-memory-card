87e34cb335bcaddd74916a13158dca17d786e525
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-11 07:19:39 +0300

    Fetch MAX_ENTRIES of images

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function App() {
+  const MAX_ENTRIES = 10;
   const [difficulty, setDifficulty] = useState(null);
-  const [images, setImages] = useState(null);
+  const [entries, setEntries] = useState([]);
   const [counter, setCounter] = useState(0);
 
   function updateDifficulty(num) {
@@ -16,15 +17,33 @@ export default function App() {
     console.log(num);
   }
 
+  function randomizer(max) {
+    return Math.floor(Math.random() * max);
+  }
+
   useEffect(() => {
-    const getPokemon = async () => {
-      try {
-        const result = await fetch(`https://pokeapi.co/api/v2/pokemon/1/`);
-        const response = await result.json();
-        setImages(response.sprites.front_default);
-      } catch (err) {
-        console.error("Failed API fetch : ", err);
+    const uniqueId = [];
+    while (uniqueId.length !== 10) {
+      const randomNumber = randomizer(150);
+      if (!uniqueId.includes(randomNumber)) {
+        uniqueId.push(randomNumber);
       }
+    }
+
+    const getPokemon = async () => {
+      const newEntries = [];
+      for (let number of uniqueId) {
+        try {
+          const result = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${number}/`
+          );
+          const response = await result.json();
+          newEntries.push(response.sprites.front_default);
+        } catch (err) {
+          console.error("Failed API fetch : ", err);
+        }
+      }
+      setEntries(newEntries);
     };
 
     getPokemon();
@@ -37,10 +56,10 @@ export default function App() {
       </div>
 
       <div className="card-table">
-        <Card src={images ? images : ""} />
-        <Card />
-        <Card />
-        <Card />
+        <Card src={entries ? entries[0] : ""} />
+        <Card src={entries ? entries[1] : ""} />
+        <Card src={entries ? entries[2] : ""} />
+        <Card src={entries ? entries[3] : ""} />
       </div>
     </>
   );
